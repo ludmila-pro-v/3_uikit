cc4aaaec2d6a31747c36c379afa727e0ef080819
ludmila-pro <luma02@mail.ru>
2021-06-21 11:25:51 +0500

    feat: fixed bags of slider

--- js/main.js
+++ js/main.js
@@ -1,150 +1,78 @@
 
-document.addEventListener("DOMContentLoaded", () => {
+// Custom slider
+// width 1 handle
 
-  // ======== Slider with one handle
-  
-  const priceSlider = document.getElementById('r-slider');
+var sliders = document.getElementsByClassName('sliders');
 
-  noUiSlider.create(priceSlider, {
-      start: [30, 150],
-      tooltips: false,
-      connect: true,
-      padding: 6,
-      range: {
-          'min': 0,
-          'max': 200
-      },
-      // pips: {
-      //     mode: 'values',
-      //     values: [50, 100, 150],
-      //     density: 4
-      // }
-  });
+for ( var i = 0; i < sliders.length; i++ ) {
 
-  priceSlider.noUiSlider.on('change', (values, handle) => {
-      goSearch();
-  });
+    noUiSlider.create(sliders[i], {
+        start: 100,
+        step: 1,
+        tooltips: true,
+        connect: [true, false],
+        padding: 6,
+        orientation: "horizontal",
+        range: {
+            'min': 0,
+            'max': 200
+        },
+    });
 
-  // ======== Slider with two handles
-  
-  const discountSlider = document.getElementById('m-slider');
+    sliders[i].noUiSlider.on('slide', addValues);
+}
 
-  noUiSlider.create(discountSlider, {
-      start: 50,
-      tooltips: true,
-      connect: [true, false],
-      step: 1,
-      range: {
-          'min': 0,
-          'max': 100
-      },
-      // format: {
-      //     to: function (value) {
-      //         return parseInt(value);
-      //     },
-      //     from: function (value) {
-      //         return parseInt(value);
-      //     }
-      // }
-  });
+function addValues(){
+    var allValues = [];
 
-  discountSlider.noUiSlider.on('change', (values, handle) => {
-      goSearch();
-  });
+    for (var i = 0; i < sliders.length; i++) {
+        allValues.push(sliders[i].noUiSlider.get());
+    };
+    
+    for (var j = 0; j < allValues.length; j++) {
+    document.getElementsByClassName('slider-value')[j].innerHTML = allValues[j];
+    }
 
-  // ======== Search filters
-  
-  function goSearch() {
-      let winHref = window.location.href.split('?')[0];
-      winHref += `?pricerange=${priceSlider.noUiSlider.get()}`;
-      winHref += `&mindiscount=${discountSlider.noUiSlider.get()}`;
-      window.location.href = winHref;
-  }
+}
 
-  // ======== Slider reset
-  
-  // const resetButton = document.getElementById('reset');
-  // resetButton.onclick = (e) => {
-  //     priceSlider.noUiSlider.reset();
-  //     discountSlider.noUiSlider.reset();
-  // };
+// width 2 handles
 
-  // ======== Slider set
+var slidersOne = document.getElementsByClassName('sliders-1');
 
- 
-  const params = new URLSearchParams(window.location.search);
-  const minDiscount = params.get("mindiscount");
-  const priceRange = params.get("pricerange");
- 
+for ( var i = 0; i < slidersOne.length; i++ ) {
 
-  if (minDiscount) {
-      discountSlider.noUiSlider.set(parseInt(minDiscount));
-  }
-  if (priceRange) {
-      priceSlider.noUiSlider.set(priceRange.split(','));
-  }
+    noUiSlider.create(slidersOne[i], {
+        start: [30, 100],
+        step: 1,
+        tooltips: false,
+        connect: true,
+        padding: 6,
+        orientation: "horizontal",
+        range: {
+            'min': 0,
+            'max': 200
+        },
+    });
 
-});
+    slidersOne[i].noUiSlider.on('slide', addValues);
+}
 
+function addValues(){
+    var allValues = [];
 
-// 3-d slider
+    for (var i = 0; i < slidersOne.length; i++) {
+        allValues.push(slidersOne[i].noUiSlider.get());
+    };
+    
+    for (var j = 0; j < allValues.length; j++) {
+    document.getElementsByClassName('slider-value')[j].innerHTML = allValues[j];
+    }
 
-(function () {
-  'use strict';
+}
 
-  var init = function () {
-
-      var slider2 = new rSlider({
-          target: '#slider2',
-          values: [0, 1, 2, 3, 4, 5, 6, '7', 8],
-          range: false,
-          set: [5],
-          tooltip: false,
-          onChange: function (vals) {
-              console.log(vals);
-          }
-      });
-
-      var slider3 = new rSlider({
-          target: '#slider3',
-          values: {
-              min: 0,
-              max: 100
-          },
-          step: 10,
-          range: true,
-          set: [10, 40],
-          scale: true,
-          labels: false,
-          onChange: function (vals) {
-              console.log(vals);
-          }
-      });
-
-      var slider = new rSlider({
-          target: '#slider',
-          values: [2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015],
-          range: true,
-          set: [2010, 2013],
-          onChange: function (vals) {
-              console.log(vals);
-          }
-      });
-
-      var mySlider = new rSlider({
-          target: '#sampleSlider',
-          values: [2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015],
-          range: true,
-          tooltip: true,
-          scale: true,
-          labels: true,
-          set: [2010, 2013]
-      });
-  };
-  window.onload = init;
-})();
 
 // End of custom slider
+
 
 
 
